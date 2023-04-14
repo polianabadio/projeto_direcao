9b13e3bde09ec1bde55cf941ffb143bf145afa1e
Poliana Ferreira Abadio <poliana.abadio@crp.com>
2023-04-14 17:38:19 -0300

    modal1

---
+++ login.js
@@ -0,0 +1,42 @@
+function logar (){
+
+    var Login = document.getElementById('Login').value;
+    var senha = document.getElementById('senha').value;
+    
+    if(Login == "matheussalessales@gmail.com" && senha =="260920"){
+        alert ('Usuário Logado');
+        location.href ="index.html";
+    
+    }else{
+    
+        alert ('usuário ou senha incorretos')
+    
+    
+    }}
+
+    
+
+
+    function mostrar() {
+        
+        const modal = document.getElementById('modal-login');
+        modal.style.display = "block";
+      }
+      
+      function ocultar () {
+        const modal = document.getElementById('modal-login');
+  
+        modal.style.display = "none";
+        
+      }
+
+      fetch('nav-bar.html')
+     .then(response => response.text())
+     .then(data => {
+        const navbar = document.createElement('nav');
+        navbar.innerHTML = data;
+        document.body.insertBefore(navbar, document.body.firstChild);
+  });
+
+
+      
